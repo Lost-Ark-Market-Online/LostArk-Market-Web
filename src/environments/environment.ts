10f649fec234aa0c4dddc00a2bb5e529acf61e44
Gogodr <gogodr@hotmail.com>
2022-07-17 17:36:56 -0500

    add the environment variables back into the project

---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'lostarkmarket-79ddf',
+    appId: '1:694220529164:web:52522f965a9c762e49d829',
+    storageBucket: 'lostarkmarket-79ddf.appspot.com',
+    locationId: 'us-east1',
+    apiKey: 'AIzaSyBMTA0A2fy-dh4jWidbAtYseC7ZZssnsmk',
+    authDomain: 'lostarkmarket-79ddf.firebaseapp.com',
+    messagingSenderId: '694220529164',
+    measurementId: 'G-TNPV3RVV4K',
+  },
+  production: false,
+  apiHost: "/api"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
